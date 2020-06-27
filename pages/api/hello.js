264807853de4af4886d27c0d8ca81fb2e9016710
tomotomy <tomo1999724@gmail.com>
2020-06-27 17:59:01 +0900

    add graphql mutation to api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,11 @@
 import Cors from 'cors'
 import initMiddleware from '../../lib/init-middleware'
 
+import client from '../../lib/apolloClient';
+import {
+  ADD_METAFIELD
+} from '../../lib/mutations';
+
 // Initialize the cors middleware
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
@@ -14,6 +19,28 @@ export default async function handler(req, res) {
   // Run cors
   await cors(req, res)
   // Rest of the API logic
-  
-  res.json(req);
+  const AddFormVariables = function(productId) {
+    return {
+      input : {
+        id: `gid://shopify/Product/${productId}`,
+        metafields: [
+          {
+            namespace: "MenkReview",
+            key: "review", // customerId
+            value: Json.stringify(req),
+            valueType: "STRING"
+          }
+        ]
+      }
+    }
+  } 
+
+  client.mutate({
+    mutation: ADD_METAFIELD,
+    variables: AddFormVariables,
+  }).then((result) => {
+    return res.json({ 
+      result: req,
+    })
+  })
 }
