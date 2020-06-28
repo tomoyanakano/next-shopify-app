0c6627cbe77e4de0504edcd6cbf70684ec13c83b
tomotomy <tomo1999724@gmail.com>
2020-06-28 19:04:10 +0900

    change api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,30 @@
 import Cors from 'cors'
 import initMiddleware from '../../lib/init-middleware'
-import { client } from '../../lib/apolloClient';
+// import { client } from '../../lib/apolloClient';
 import {
   ADD_METAFIELD
 } from '../../lib/mutations';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+const ApolloClient = require('apollo-boost').default;
 
+
+const cache = new InMemoryCache()
+const link = new HttpLink({
+  uri: "https://menkapp.myshopify.com/api/graphql",
+  headers: `X-Shopify-Access-Token: f9801b3ca3e6321020f7bc00cbbdfcaa`
+})
+
+export const client = new ApolloClient({
+  cache: cache,
+  link: link,
+  name: 'react-web-client',
+  version: '1.3', 
+  queryDeduplication: false,
+  fetchOptions: {
+    credentials: 'include',
+  },
+})
 // Initialize the cors middleware
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
