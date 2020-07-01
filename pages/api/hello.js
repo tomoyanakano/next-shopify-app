e1756258761804779afd326c7111e5559fbb169a
tomotomy <tomo1999724@gmail.com>
2020-07-01 17:30:34 +0900

    change api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -61,7 +61,9 @@ export default async function handler(req, res) {
     body: JSON.stringify(data)
   };
 
-  const url = 'https://' + SHOPIFY_API_KEY + ':' + SHOPIFY_API_SECRET_KEY + '@menkapp.myshopify.com/admin/api/2020-04/products/'+ req.body.productId +'/metafields.json'
+  // /admin/api/2020-04/metafields/count.json
+  // /admin/api/2020-04/products/'+ req.body.productId +'/metafields.json
+  const url = 'https://' + SHOPIFY_API_KEY + ':' + SHOPIFY_API_SECRET_KEY + '@menkapp.myshopify.com/admin/api/2020-04/metafields/count.json'
   fetch(url, optionsMetafields)
     .then(res => res.json())
     .then(response => {
