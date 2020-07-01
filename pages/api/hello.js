47d248f5c897f2c64983936aceaab67879889462
tomotomy <tomo1999724@gmail.com>
2020-07-01 14:57:02 +0900

    change api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -48,11 +48,11 @@ export default async function handler(req, res) {
     headers: {
       "Content-Type": "application/json"
     },
-    body: params
+    body: JSON.stringify(params)
   };
 
   const url = 'https://' + SHOPIFY_API_KEY + ':' + SHOPIFY_API_SECRET_KEY + '@menkapp.myshopify.com/admin/api/2019-07/graphql.json'
-  fetch(url, JSON.stringify(optionsMetafields))
+  fetch(url, optionsMetafields)
     .then(res => res.json())
     .then(response => {
       return res.json({
