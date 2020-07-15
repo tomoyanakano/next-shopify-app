64442f80aa0e8c5f69420c77cbf4a62784aac4e0
tomotomy <tomo1999724@gmail.com>
2020-07-15 17:33:11 +0900

    change api

--- pages/api/reviewSummary.js
+++ pages/api/reviewSummary.js
@@ -1,44 +1,48 @@
-import Cors from 'cors'
-import initMiddleware from '../../lib/init-middleware'
-const dotenv = require('dotenv');
-const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY, SHOPIFY_PRIVATE_APP_API, SHOPIFY_PRIVATE_APP_PASS } = process.env;
+// import Cors from 'cors'
+// import initMiddleware from '../../lib/init-middleware'
+// const dotenv = require('dotenv');
+// const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY, SHOPIFY_PRIVATE_APP_API, SHOPIFY_PRIVATE_APP_PASS } = process.env;
 
-// Initialize the cors middleware
-const url = 'https://'+ SHOPIFY_PRIVATE_APP_API + ':' + SHOPIFY_PRIVATE_APP_PASS + '@menkapp.myshopify.com/admin/api/2020-04'
-const cors = initMiddleware(
-  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-  Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ['GET', 'POST', 'OPTIONS'],
-  })
-)
+// // Initialize the cors middleware
+// const url = 'https://'+ SHOPIFY_PRIVATE_APP_API + ':' + SHOPIFY_PRIVATE_APP_PASS + '@menkapp.myshopify.com/admin/api/2020-04'
+// const cors = initMiddleware(
+//   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+//   Cors({
+//     // Only allow requests with GET, POST and OPTIONS
+//     methods: ['GET', 'POST', 'OPTIONS'],
+//   })
+// )
 
-export default async function handler(req, res) {
-  // Run cors
-  await cors(req, res)
+// export default async function handler(req, res) {
+//   // Run cors
+//   await cors(req, res)
 
-  const data = {
-    "metafield": {
-      "namespace": "reviewSummary",
-      "key": "summary",
-      "value": JSON.stringify(req.body),
-      "value_type": "json_string"
-    }
-  }
+//   const data = {
+//     "metafield": {
+//       "namespace": "reviewSummary",
+//       "key": "summary",
+//       "value": JSON.stringify(req.body),
+//       "value_type": "json_string"
+//     }
+//   }
 
-  const optionsMetafields = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data)
-  };
+//   const optionsMetafields = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data)
+//   };
 
-  fetch(url + '/products/'+ req.body.productId +'/metafields.json', optionsMetafields)
-    .then(res => res.json())
-    .then(response => {
-      return res.json({
-        result: response,
-      })
-    });
+//   fetch(url + '/products/'+ req.body.productId +'/metafields.json', optionsMetafields)
+//     .then(res => res.json())
+//     .then(response => {
+//       return res.json({
+//         result: response,
+//       })
+//     });
+// }
+
+export default (req, res) => {
+  res.status(200).json({ text: 'Hello' })
 }
